09f9c17f4a1bf09da34f0ec0a02bb78c7f35cbf0
Creed Camp <creedomed@gmail.com>
2020-09-27 21:59:11 -0700

    re-assignment 2.0

--- main.js
+++ main.js
@@ -2,39 +2,38 @@
 //step two:convert to arr
 //step three: convert to string
 //step four: input to html
+
+const goTo = document.querySelector("#todos")
+
+function whichIsIt(todos) {
+  const itsThose = todos.map((item) => {
+    return `<li><strong>id</strong>: ${item.id}, <strong>title</strong>: ${item.title}, <strong>completed</strong>: ${item.completed}</li>`
+  })
+  const pullOne = itsThose.join("")
+  goTo.innerHTML = pullOne
+}
+
+// function whatIsIt(todos) {
+//   const itsThis = todos.map((item) => {
+//     return `<li>${item.title}</li>`
+//   })
+//   const pullTwo = itsThis.join("")
+//   goTo.innerHTML = pullTwo
+// }
+
+// function isItDone(todos) {
+//   const findOut = todos.map((item) => {
+//     return `<li>${item.completed}</li>`
+//   })
+//   const pullThree = findOut.join("")
+//   goTo.innerHTML = pullThree
+// }
 fetch(`https://jsonplaceholder.typicode.com/todos`)
   .then((resp) => resp.json())
-  .then((itemObj) => {
-    console.log(itemObj)
-    return fetch(itemObj.userId)
-  })
-  .then((resp) => resp.json())
-  .then((userId) => {
-    console.log(userId)
-    return fetch(userId[0])
-  })
-  .then((resp) => resp.json())
-  .then((id) => {
-    console.log(id)
-    return fetch(id[0])
-  })
-  .then((resp) => resp.json())
-  .then((title) => {
-    return fetch(title[0])
-  })
-  .then((resp) => resp.json())
-  .then((completed) => {
-    return fetch(completed[0])
-  })
+  .then(whichIsIt)
 
 //not really sure what to apply my .map to here, but I know .join comes after one the last parenthases.
-let html = userId
-  .map((userId) => {
-    return `<li>${(userId[0], id[0], title[0], completed[0])}</li>`
-  })
-  .join("")
-
-document.querySelector(html).innerHTML("boo")
+// let html = userId
 
 //const prom = new Promis((resolve, reject) => {
 //  setTimeout(() =>)
@@ -48,3 +47,4 @@ document.querySelector(html).innerHTML("boo")
 // the fetch method would start the promise for me, allowing me to use .then immediately after.
 //Also, I kept having an issue where the browser wanted to know what a setting was toward it being an external or not, and
 //and I figured don't use catch, cause it might not help, and we haven't covered it, so it's likely not the answer.
+//However, it did not let me check my work due to that error.
